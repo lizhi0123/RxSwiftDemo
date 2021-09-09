f986f78b908ca1b0926a0b9169f2ec44290018c5
ZhangZhili <1056522750@qq.com>
2021-09-09 17:22:01 +0800

    feat:  首页数据

--- LZRxSwiftDemo/ViewController.swift
+++ LZRxSwiftDemo/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    var items = Observable.just(  ["RxSwift tableView刷新","b","c"] )
+    
+    lazy var tableView: UITableView = {
+        let table = UITableView(frame: view.bounds)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(tableView)
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.title = "荔枝的RxSwiftDemo"
+        setupTable()
+        setupNavBar()
     }
-
-
+    
+    func setupTable()  {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        items
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element)"
+                cell.accessoryType = .disclosureIndicator
+            }
+            .disposed(by: rx.disposeBag)
+        
+        tableView.rx.itemSelected.subscribe(onNext: { indexpath in
+            print("- row =" + String( indexpath.row))
+        }).disposed(by: rx.disposeBag)
+    }
+    func setupNavBar() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "刷新", style: UIBarButtonItem.Style.plain, target: self, action: #selector(Self.refreshClick))
+    }
+    @objc func refreshClick()  {
+         items = Observable.just(  ["OC","Swift"] )
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '13.0'
+
+target 'LZRxSwiftDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LZRxSwiftDemo
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'RxDataSources'
+#  https://github.com/RxSwiftCommunity/NSObject-Rx
+  pod 'NSObject+Rx'
+
+  target 'LZRxSwiftDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'LZRxSwiftDemoUITests' do
+    # Pods for testing
+  end
+
+end
